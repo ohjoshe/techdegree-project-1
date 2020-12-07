83f9f4770b86ea4db18106f0b189aab5267db53b
Josh Karp <joshua.karp@icloud.com>
2020-12-07 16:55:36 -0500

    script.js updated

--- js/script.js
+++ js/script.js
@@ -10,18 +10,18 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-let quotes = [
+let quotesArray = [
   {
     quote: 'I reject your reality and substitute my own.',
     source: 'Adam Savage',
     citation: 'Mythbusters',
-    year: ''
+    year: undefined
   },
   {
     quote: 'The human imagination is a secret place of primitive memories and unconfessed desires.',
     source: 'Marius',
     citation: 'The Vampire Lestat by Anne Rice',
-    year: ''
+    year: undefined
   },
   {
     quote: "You can't let your failures define you - you have to let them teach you. You have to let them show you what to do differently next time.",
@@ -32,20 +32,20 @@ let quotes = [
   {
     quote: 'Every kid starts out as a natural-born scientist, and then we beat it out of them. A few trickle through the system with their wonder and enthusiasm for science intact.',
     source: 'Carl Sagan',
-    citation: '',
+    citation: undefined,
     year: '1996'
   },
   {
     quote: 'I you want your children to be intelligent, read them fairy tales. If you want them to be more intelligent, read them more fairy tales.',
     source: 'Albert Einstein',
-    citation: '',
-    year: ''
+    citation: undefined,
+    year: undefined
   },
   {
     quote: 'I only know that without you, no rank or fame would be worth having.',
     source: 'Stefen',
     citation: "Magic's Price by Mercedes Lackey",
-    year: ''
+    year: undefined
   },
 ];
 
@@ -53,12 +53,31 @@ let quotes = [
 /***
  * `getRandomQuote` function
 ***/
-
-
+function getRandomQuote(array) {
+  let randomIndex = Math.floor(Math.random() * Math.floor(array.length))
+  return quotesArray[randomIndex]
+}
 
 /***
  * `printQuote` function
 ***/
+function printQuote() {
+  let quoteObj = getRandomQuote(quotesArray)
+  let html = `
+    <p class="quote">${quoteObj.quote}</p>
+    <p class="source">${quoteObj.source}
+    `
+  
+  if (quoteObj.citation !== undefined) {
+    html += `<span class="citation">${quoteObj.citation}</span>`
+  }
+  if (quoteObj.year !== undefined) {
+    html += `<span class="year">${quoteObj.year}</span>`
+  }
+  html += `</p>`
+
+  return document.getElementById('quote-box').innerHTML = html;
+}
 
 
 
