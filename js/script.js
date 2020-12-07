6b69aeabadcdf7c9fdb074bf985352146a916fa3
Josh Karp <joshua.karp@icloud.com>
2020-12-07 16:03:54 -0500

    Project File Upload

---
+++ js/script.js
@@ -0,0 +1,70 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+let quotes = [
+  {
+    quote: 'I reject your reality and substitute my own.',
+    source: 'Adam Savage',
+    citation: 'Mythbusters',
+    year: ''
+  },
+  {
+    quote: 'The human imagination is a secret place of primitive memories and unconfessed desires.',
+    source: 'Marius',
+    citation: 'The Vampire Lestat by Anne Rice',
+    year: ''
+  },
+  {
+    quote: "You can't let your failures define you - you have to let them teach you. You have to let them show you what to do differently next time.",
+    source: 'President Barack Obama',
+    citation: "National Address to America's Schoolchildren",
+    year: '2009'
+  },
+  {
+    quote: 'Every kid starts out as a natural-born scientist, and then we beat it out of them. A few trickle through the system with their wonder and enthusiasm for science intact.',
+    source: 'Carl Sagan',
+    citation: '',
+    year: '1996'
+  },
+  {
+    quote: 'I you want your children to be intelligent, read them fairy tales. If you want them to be more intelligent, read them more fairy tales.',
+    source: 'Albert Einstein',
+    citation: '',
+    year: ''
+  },
+  {
+    quote: 'I only know that without you, no rank or fame would be worth having.',
+    source: 'Stefen',
+    citation: "Magic's Price by Mercedes Lackey",
+    year: ''
+  },
+];
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
